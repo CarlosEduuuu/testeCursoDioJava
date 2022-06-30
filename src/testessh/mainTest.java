609226a6c182701d2d678d564de6e55aabfa4671
Carlos <carlosfelixedu@hotmail.com>
2022-06-30 15:44:59 -0300

    primeiro commit

---
+++ src/testessh/mainTest.java
@@ -0,0 +1,11 @@
+package testessh;
+
+public class mainTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Teste github ssh!");
+
+	}
+
+}
